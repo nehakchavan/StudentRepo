9b94929938768ad7a3661d095940a080dcb61392
nehakchavan <89125952+nehakchavan@users.noreply.github.com>
2023-02-20 14:50:19 +0530

    Update StudentApplicationTests.java

--- student/src/test/java/com/cybage/StudentApplicationTests.java
+++ student/src/test/java/com/cybage/StudentApplicationTests.java
@@ -3,7 +3,7 @@ package com.cybage;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication
 class StudentApplicationTests {
 
 	@Test
